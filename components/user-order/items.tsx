262918d1cf021c9df425ef467da4335909c5999c
devvspaces <netrobepy@gmail.com>
2022-11-07 21:54:40 +0100

    update

--- components/user-order/items.tsx
+++ components/user-order/items.tsx
@@ -1,8 +1,8 @@
-import { BookItem } from "../item/types";
 import styles from "../../styles/cart.module.css";
 import { UserOrderItem } from "./item";
+import { _CartItem } from "../../src/models/book";
 
-const UserOrderItems = ({ items }: {items: BookItem[]}) => {
+const UserOrderItems = ({ items }: {items: _CartItem[]}) => {
     return (
         <div className={styles.cartItems}>
             {items.map((item) => (
